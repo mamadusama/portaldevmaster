5130743d61b3b6284e1c39741b50ea3d44322bde
mamadusama <seu_email@example.commamadusamadev@gmail.com>
2024-11-30 04:38:33 +0000

    feat ad Criando seus templates e a estrutura de

---
+++ curso/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import include, path
+from .views import homepage
+
+urlpatterns = [
+  
+    path('',  homepage),
+] 
--- portaldevmaster/urls.py
+++ portaldevmaster/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path("/", include("curso.urls", namespace="curso")),
+    path('', include("curso.urls")),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
