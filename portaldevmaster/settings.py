dfaf7d7d675684927e054b588c4c66d8166a7a84
mamadusama <mamadusamadev@gmail.com>
2024-12-10 22:45:43 +0000

    feat add homepage

--- portaldevmaster/settings.py
+++ portaldevmaster/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt-Pt'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'Europe/Lisbon'
 
